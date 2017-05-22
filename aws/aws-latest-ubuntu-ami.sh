bca35ebbe414904ad1214457d0e125ea52bca699
Rafael Moraes <contact@moraesrafael.com>
2017-05-21 23:37:02 -0400

    locator

--- aws/aws-latest-ubuntu-ami.sh
+++ aws/aws-latest-ubuntu-ami.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://cloud-images.ubuntu.com/locator/ec2/
+
 latest_ubuntu=`aws ec2 describe-images --filters "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server*" --profile default --region ca-central-1 | jq -r '.Images[].Name' | sort -r | head -n 1`
 
 image_id=`aws ec2 describe-images --filters "Name=name,Values=$latest_ubuntu" --profile default --region ca-central-1 | jq -r '.Images[].ImageId'`
